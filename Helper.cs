b1ba2ed71c9a9ffc717a58ce2fd47673912102c5
spencer021 <spencerjolson@gmail.com>
2024-01-29 15:31:40 -0700

    Setup project

---
+++ Helper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission4
+{
+    internal class Helper
+    {
+        public string printBoard(string[] boardArray)
+        {
+            return null;
+        }
+
+        public string hasWinner(string[] boardArray)
+        {
+            return null;
+        }
+    }
+}
